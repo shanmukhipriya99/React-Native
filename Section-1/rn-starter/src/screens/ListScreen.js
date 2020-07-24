3eba1fdf9a0b40f8fc6db44d0f7a0e973994a7c3
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-24 18:31:42 +0530

    Section-3 Exercise: Added age

--- Section-1/rn-starter/src/screens/ListScreen.js
+++ Section-1/rn-starter/src/screens/ListScreen.js
@@ -3,24 +3,24 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friend #1'},
-        {name: 'Friend #2'},
-        {name: 'Friend #3'},
-        {name: 'Friend #4'},
-        {name: 'Friend #5'},
-        {name: 'Friend #6'},
-        {name: 'Friend #7'},
-        {name: 'Friend #8'},
-        {name: 'Friend #9'}
+        {name: 'Friend #1', Age: 20 },
+        {name: 'Friend #2', Age: 20 },
+        {name: 'Friend #3', Age: 20 },
+        {name: 'Friend #4', Age: 20 },
+        {name: 'Friend #5', Age: 20 },
+        {name: 'Friend #6', Age: 20 },
+        {name: 'Friend #7', Age: 20 },
+        {name: 'Friend #8', Age: 20 },
+        {name: 'Friend #9', Age: 20 }
     ]
     return (
         <FlatList 
-            horizontal    // boolean value to scroll horizontally
-            showsHorizontalScrollIndicator={false}
+            // horizontal    // boolean value to scroll horizontally
+            // showsHorizontalScrollIndicator={false}
             keyExtractor={friend => friend.name}  //A better way of assigning keys compared to hard-coding 
             data={friends} 
             renderItem={({ item }) => {   // (element) returns{item: {name: 'Friend#1'}, index: 0 } Therefore: use ({item}) directly
-                return <Text style={styles.textStyle}>{item.name}</Text>    // ({item}) returns {name: 'Friend #1'}
+                return <Text style={styles.textStyle}>{item.name}- Age {item.Age}</Text>    // ({item}) returns {name: 'Friend #1'}
             }}/>
     );
 };
