4dabeeaff51551a981c0e9ac1d01d9a39b67daa3
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-24 18:23:10 +0530

    Added content

--- Section-1/rn-starter/src/screens/ListScreen.js
+++ Section-1/rn-starter/src/screens/ListScreen.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>;
+    const friends = [
+        {name: 'Friend #1'},
+        {name: 'Friend #2'},
+        {name: 'Friend #3'},
+        {name: 'Friend #4'},
+        {name: 'Friend #5'},
+        {name: 'Friend #6'},
+        {name: 'Friend #7'},
+        {name: 'Friend #8'},
+        {name: 'Friend #9'}
+    ]
+    return (
+        <FlatList 
+            keyExtractor={friend => friend.name}  //A better way of assigning keys compared to hard-coding 
+            data={friends} 
+            renderItem={({ item }) => {   // (element) returns{item: {name: 'Friend#1'}, index: 0 } Therefore: use ({item}) directly
+                return <Text style={styles.textStyle}>{item.name}</Text>    // ({item}) returns {name: 'Friend #1'}
+            }}/>
+    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
 
 export default ListScreen;
