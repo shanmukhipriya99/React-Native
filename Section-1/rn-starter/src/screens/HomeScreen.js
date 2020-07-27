09e7c53325cafecce9f614a509aba3dd73fef301
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-27 19:47:36 +0530

    navigation

--- Section-1/rn-starter/src/screens/HomeScreen.js
+++ Section-1/rn-starter/src/screens/HomeScreen.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = (props) => {  //can use destructuring to replace (props) with ({ navigation })
   return (
     <View>
       <Text style={styles.text}>Hi there!</Text>
       <Button 
         title='Go to Components Demo'
-        onPress={() => props.navigation.navigate('Components')}/>
-      <TouchableOpacity onPress={() => console.log('List Pressed!')}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+        onPress={() => props.navigation.navigate('Components')}/>  
+        <Button 
+        title='Go to List Demo'
+        onPress={() => props.navigation.navigate('List')}/>
     </View>
   );
 };
