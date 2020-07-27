a94dd5e9742bebe762ffdc87819f2d20cb73b15d
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-27 20:33:17 +0530

    Image detail component

--- Section-1/rn-starter/src/screens/ImageScreen.js
+++ Section-1/rn-starter/src/screens/ImageScreen.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native' ;
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
-    return <Text>ImageScreen</Text>;
-}
+  return <View>
+      <ImageDetail />
+      <ImageDetail />
+      <ImageDetail />
+  </View>
+};
 
 const styles = StyleSheet.create({});
 
